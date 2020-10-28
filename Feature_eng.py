7e96d833f348fd4b27a6b39db0b41788b9e056e4
GitHubUser97 <64754902+GitHubUser97@users.noreply.github.com>
2020-10-28 11:25:08 +0100

    ss

--- Feature_eng.py
+++ Feature_eng.py
@@ -1,5 +1,3 @@
-
-
 def weighted_avg_ds(data, weight_col=False, weight_inplace=False,
                     weights_dict={"Madrid":1/6,"Barcelona":1/6,"Valencia":1/6,"Seville":1/6,"Zaragoza":1/6,"Malaga":1/6 }):
     
@@ -7,51 +5,77 @@ def weighted_avg_ds(data, weight_col=False, weight_inplace=False,
     Weights for each city  need to be specified in the weights_dict, if weights are equal (1/6), it will just average everything out normally
     If weight_col =  True, it will add 6 separate columns with choosen weights
     If weight_inplace = True it will multiply the weights by all the features for each city separately, but not for the target var 
+
+    Binary cloud cover transformation is factored in this function regardless of the args passed
     """
-    col_ends = ["_d2m", "_t2m","_i10fg", "_sp", "_tcc", "_tp"]
-    
+    col_ends = ["_d2m", "_t2m","_i10fg", "_sp", "_tcc", "_tp"]    
     df = pd.DataFrame()
+    #### Adding the power consumption col if present in data ### 
     if 'VolumeMWh' in data.columns:
         df['VolumeMWh'] = data['VolumeMWh']
+        
     if weight_inplace == False:
     
         if weight_col == False:
 
             for col in col_ends:
                 a = 0 
+                
+                if not col.endswith("tcc"):
+                    for city in weights_dict.keys():
 
-                for city in weights_dict.keys():
+                        a+=1
+                        if a == 1:
 
-                    a+=1
-                    if a == 1:
-
-                        df[col[1:]] = data[city+col]*(weights_dict[city])
-                        continue
-                    colum = data[city+col]*weights_dict[city]
-                    df[col[1:]] = df[col[1:]]+colum
+                            df[col[1:]] = data[city+col]*(weights_dict[city])
+                            continue
+                        colum = data[city+col]*weights_dict[city]
+                        df[col[1:]] = df[col[1:]]+colum
+                        
+                elif col.endswith("tcc"):
                     
+                    for city in weights_dict.keys():
+
+                        a+=1
+                        if a == 1:
+
+                            df[col[1:]] = data[city+col].apply(bin) * weights_dict[city]
+                            continue
+                        colum = data[city+col].apply(bin) * weights_dict[city]
+                        df[col[1:]] = df[col[1:]]+colum
+                    ### rounding tcc to 0 or 1 ####     
+                    df[col[1:]] = df[col[1:]].apply(round).astype("int64")              
         elif weight_col == True:
             df = data
             for city in weights_dict.keys():
                 df[city+"_weight"] = round(weights_dict[city],3)
-                
+            ## apply the binary col for all the cols ending with tcc ###
+            for col in df.columns:
+                if col.endswith("tcc"):
+                    df[col]=df[col].apply(bin)
+                    
     elif weight_inplace == True and weight_col==False:
         
          for city in weights_dict.keys():
             for column in data.columns:
                 if column.startswith(city):
+                    if column.endswith("tcc"):
+                        df[column] = data[column].apply(bin)
+                        continue
                     df[column] = data[column]*weights_dict[city]
                     continue
     return df
+
+
             
 
- ####### Clouds binary func cutting at 0.1 and 0.9 #######
+ ####### Clouds binary func cutting at 0.8 #######
 
  def bin(x):
-    if x < 0.1:
-        return 0
-    elif x > 0.9:
-        return 1 
+    if x > 0.8:
+        return 1
+    else:
+        return 0 
 def binary_tcc(data):
     df = data
     for col in data.columns:
