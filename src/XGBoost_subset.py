55f8432a969db2738cf9f2177945906941df1b24
fritz-max <fritz.doermann@gmail.com>
2020-10-29 20:08:00 +0100

    modified Subset file and added make predictions file

--- src/XGBoost_subset.py
+++ src/XGBoost_subset.py
@@ -17,18 +17,18 @@ sns.set()
 X_TRAIN_PATH = '../Case_material/train/X_train.csv'
 Y_TRAIN_PATH = '../Case_material/train/y_train.csv'
 
-X = pd.read_csv(X_TRAIN_PATH)
-y = pd.read_csv(Y_TRAIN_PATH)
+X_train = pd.read_csv(X_TRAIN_PATH)
+y_train = pd.read_csv(Y_TRAIN_PATH)
 
-add_time(X)
-add_hour_dayofweek_month(X)
-add_hour_batches(X)
-add_weekend(X)
-add_business_hour(X)
-add_siesta(X)
-add_holidays_spain(X)
-add_city_weight(X)
-normalize(X)
+add_time(X_train)
+add_hour_dayofweek_month(X_train)
+add_hour_batches(X_train)
+add_weekend(X_train)
+add_business_hour(X_train)
+add_siesta(X_train)
+add_holidays_spain(X_train)
+add_city_weight(X_train)
+normalize(X_train)
 
 cols2incl = [
     "hour",
@@ -36,10 +36,10 @@ cols2incl = [
     "dayofyear",
     "Madrid_t2m",
     "holidays",
-    "Malaga_t2m",
-    "Malaga_d2m",
-    "weekofyear",
-    "hour3"
+    "Malaga_t2m"
+    # "Malaga_d2m",
+    # "weekofyear",
+    # "hour3",
     # "Seville_t2m",
     # "Seville_tp",
     # "quarter",
@@ -58,76 +58,55 @@ cols2incl = [
     # "Malaga_sp",
 ]
 
-cols2drop = ['time', 'ValueDateTimeUTC', 'Madrid_d2m', 'Madrid_t2m', 'Madrid_i10fg',
-             'Madrid_sp', 'Madrid_tcc', 'Madrid_tp', 'Barcelona_d2m',
-             'Barcelona_t2m', 'Barcelona_i10fg', 'Barcelona_sp', 'Barcelona_tcc',
-             'Barcelona_tp', 'Valencia_d2m', 'Valencia_t2m', 'Valencia_i10fg',
-             'Valencia_sp', 'Valencia_tcc', 'Valencia_tp', 'Seville_d2m',
-             'Seville_t2m', 'Seville_i10fg', 'Seville_sp', 'Seville_tcc',
-             'Seville_tp', 'Zaragoza_d2m', 'Zaragoza_t2m', 'Zaragoza_i10fg',
-             'Zaragoza_sp', 'Zaragoza_tcc', 'Zaragoza_tp', 'Malaga_d2m',
-             'Malaga_t2m', 'Malaga_i10fg', 'Malaga_sp', 'Malaga_tcc', 'Malaga_tp']
-
-
 # X.drop(columns=['ValueDateTimeUTC', 'time', 'date'], inplace=True)
-X = X[cols2incl]
-y.drop(columns=['ValueDateTimeUTC'], inplace=True)
-
-# xgb_model = xgb.XGBRegressor()
-# param_search = {
-#     'max_depth': randint(5, 20),
-#     'learning_rate': loguniform(1e-4, 1e0)}
+X_train = X_train[cols2incl]
+y_train.drop(columns=['ValueDateTimeUTC'], inplace=True)
 
 split_frac = 0.9
-X_train = X[:int(X.shape[0]*split_frac)]
-X_test = X[int(X.shape[0]*split_frac):]
-y_train = y[:int(X.shape[0]*split_frac)]
-y_test = y[int(X.shape[0]*split_frac):]
+X_trainset = X_train[:int(X_train.shape[0]*split_frac)]
+X_valset = X_train[int(X_train.shape[0]*split_frac):]
+y_trainset = y_train[:int(X_train.shape[0]*split_frac)]
+y_valset = y_train[int(X_train.shape[0]*split_frac):]
 
-n_estimators = 2000
-# params = {
-#     # Parameters that we are going to tune.
-#     'learning_rate': 0.13872832647046318,
-#     'max_depth': 7,
-#     # Other parameters
-#     'objective': 'reg:squarederror'
-# }
+n_estimators = 81
+params = {
+    # Parameters that we are going to tune.
+    'learning_rate': 0.14,
+    'max_depth': 7,
+    # Other parameters
+    'objective': 'reg:squarederror'
+}
 
-# xgb_model = XGBRegressor(
-#     **params,
-#     n_estimators=n_estimators,
-#     n_jobs=8)
+xgb_model = XGBRegressor(
+    **params,
+    n_estimators=n_estimators,
+    n_jobs=8)
 
-split_frac = 0.9
-X_train = X[:int(X.shape[0]*split_frac)]
-X_test = X[int(X.shape[0]*split_frac):]
-y_train = y[:int(X.shape[0]*split_frac)]
-y_test = y[int(X.shape[0]*split_frac):]
+fit = xgb_model.fit(
+    X_trainset,
+    y_trainset,
+    eval_set=[(X_trainset, y_trainset), (X_valset, y_valset)],
+    early_stopping_rounds=25)
 
-i = 1
-score = []
-
-rf = RandomForestRegressor(
-    n_estimators=1000, random_state=42)
-# Train the model on training data
-fit = rf.fit(X_train, y_train)
-
-test_preds = fit.predict(X_test)
 
 # fit = xgb_model.fit(
 #     X_train,
-#     y_train,
-#     eval_set=[(X_train, y_train), (X_test, y_test)],
-#     early_stopping_rounds=25)
+#     y_train)
 
+# fit.save_model('0001.model')
 
-# test_preds = xgb_model.predict(X_test)
+test_preds = xgb_model.predict(X_valset)
 
-print('MSE:', round(mean_squared_error(y_test.to_numpy(),
+print('MSE:', round(mean_squared_error(y_valset.to_numpy(),
                                        test_preds.reshape((-1, 1))), 2))
-print('R^2:', round(r2_score(y_test.to_numpy(), test_preds.reshape((-1, 1))), 2))
+print('R^2:', round(r2_score(y_valset.to_numpy(), test_preds.reshape((-1, 1))), 2))
 
-mape = np.mean(np.abs((y_test.to_numpy() - test_preds.reshape((-1, 1))) /
-                      np.abs(y_test.to_numpy())))
+mape = np.mean(np.abs((y_valset.to_numpy() - test_preds.reshape((-1, 1))) /
+                      np.abs(y_valset.to_numpy())))
 print('Mean Absolute Percentage Error (MAPE):', round(mape * 100, 2))
 print('Accuracy:', round(100*(1 - mape), 2))
+
+plt.figure(figsize=(16, 6))
+plt.plot(y_valset.to_numpy())
+plt.plot(test_preds)
+plt.show()
